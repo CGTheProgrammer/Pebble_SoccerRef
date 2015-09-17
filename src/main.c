2b8408853358b777e403c2eadf36f3153ee0982d
Cristian Gonzalez <cris.rules@icloud.com>
2015-09-17 12:50:14 -0700

    Fixed Numbers Showing Same Number
    - Added two iota functions so as to each deal with on teams score
      - Can probably fix it so as to only have to use one, will figure it out later

--- src/main.c
+++ src/main.c
@@ -8,16 +8,64 @@ static TextLayer *team2_score_text_layer;
 int team1Score;
 int team2Score;
 
+char *itoa1(int num){
+  static char buff[20] = {};
+  int i = 0, temp_num = num, length = 0;
+  char *string = buff;
+
+  if(num > 0) {  
+		// count how many characters in the number
+    while(temp_num) {
+      temp_num /= 10;
+      length++;
+    }
+    // assign the number to the buffer starting at the end of the number
+    for(i = 0; i < length; i++) {
+      buff[(length-1)-i] = '0' + (num % 10);
+      num /= 10;
+    }
+    buff[i] = '\0'; // can't forget the null byte to properly end our string
+  }else{
+    string = "0";
+  }
+  return string;
+}
+
+char *itoa2(int num){
+  static char buff[20] = {};
+  int i = 0, temp_num = num, length = 0;
+  char *string = buff;
+
+  if(num > 0) {  
+		// count how many characters in the number
+    while(temp_num) {
+      temp_num /= 10;
+      length++;
+    }
+    // assign the number to the buffer starting at the end of the number
+    for(i = 0; i < length; i++) {
+      buff[(length-1)-i] = '0' + (num % 10);
+      num /= 10;
+    }
+    buff[i] = '\0'; // can't forget the null byte to properly end our string
+  }else{
+    string = "0";
+  }
+  return string;
+}
+
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   
 }
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
   team1Score++;
+	text_layer_set_text(team1_score_text_layer, itoa1(team1Score));
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
   team2Score++;
+	text_layer_set_text(team2_score_text_layer, itoa2(team2Score));
 }
 
 static void click_config_provider(void *context) {
